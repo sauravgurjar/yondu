7e74dac5fef6312cd8aa4ef07114ef2fb02c1492
sauravgurjar <58848027+sauravgurjar@users.noreply.github.com>
2023-11-18 06:52:11 +0530

    saurav protfolio

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Saurav Singh</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Saurav Singh" />
+            <meta name="description"
+                content="Saurav Singh  Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Saurav Singh" />
+            <meta name="keywords"
+                content="Saurav Singh, Saurav Singh portfolio, Saurav Singh linux, ubuntu portfolio, Saurav Singh protfolio,Saurav Singh , Saurav Singh, Saurav Saurav, Saurav patel Saurav portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Saurav Singh Portfolio - Computer Engineering Student" />
+            <meta itemProp="description"
+                content="Saurav singh  Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Saurav Singh Portfolio - Computer Engineering Student" />
+            <meta name="twitter:description"
+                content="Saurav Singh's (SauravSingh) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="SauravSingh" />
+            <meta name="twitter:creator" content="SauravSingh" />
+            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Saurav Singh Portfolio - Computer Engineering Student" />
+            <meta name="og:description"
+                content="Saurav Singh's (SauravSingh) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/logo_1200.png" />
+            <meta name="og:url" content="https://github.com/sauravgurjar" />
+            <meta name="og:site_name" content="Saurav Singh Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.svg" />
+            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Ubuntu from "../components/ubuntu";
+import ReactGA from 'react-ga4';
+import Meta from "../components/SEO/Meta";
+
+
+function App() {
+  return (
+    <>
+      <Meta />
+      <Ubuntu />
+    </>
+  )
+}
+
+export default App;
